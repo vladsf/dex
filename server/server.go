5ee629c5c40ce6429b85a625102d5fe7537119c1
Zach Brown <zbrown@sevone.com>
2018-01-02 22:15:01 -0500

    Add support for password grant #926
    Co-authored-by: Rui Yang <ryang@pivotal.io>

--- server/server.go
+++ server/server.go
@@ -75,6 +75,8 @@ type Config struct {
 	RotateKeysAfter      time.Duration // Defaults to 6 hours.
 	IDTokensValidFor     time.Duration // Defaults to 24 hours
 	AuthRequestsValidFor time.Duration // Defaults to 24 hours
+	// If set, the server will use this connector to handle password grants
+	PasswordConnector string
 
 	GCFrequency time.Duration // Defaults to 5 minutes
 
@@ -143,6 +145,9 @@ type Server struct {
 
 	// If enabled, show the connector selection screen even if there's only one
 	alwaysShowLogin bool
+
+	// Used for password grant
+	passwordConnector string
 
 	supportedResponseTypes map[string]bool
 
@@ -215,6 +220,7 @@ func newServer(ctx context.Context, c Config, rotationStrategy rotationStrategy)
 		alwaysShowLogin:        c.AlwaysShowLoginScreen,
 		now:                    now,
 		templates:              tmpls,
+		passwordConnector:      c.PasswordConnector,
 		logger:                 c.Logger,
 	}
 
