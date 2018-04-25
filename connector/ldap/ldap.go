cd755fd495f86616970a290aa1476ee25ea2d8e5
Joshua Winters <jwinters@pivotal.io>
2018-04-25 10:48:42 -0400

    Use the ldap username attr for identity.username field
    Signed-off-by: Mark Huang <mhuang@pivotal.io>

--- connector/ldap/ldap.go
+++ connector/ldap/ldap.go
@@ -323,12 +323,15 @@ func (c *ldapConnector) identityFromEntry(user ldap.Entry) (ident connector.Iden
 	// TODO(ericchiang): Let this value be set from an attribute.
 	ident.EmailVerified = true
 
+	if c.UserSearch.Username != "" {
+		if ident.Username = getAttr(user, c.UserSearch.Username); ident.Username == "" {
+			missing = append(missing, c.UserSearch.Username)
+		}
+	}
+
 	if c.UserSearch.NameAttr != "" {
-		if name := getAttr(user, c.UserSearch.NameAttr); name == "" {
+		if ident.Name = getAttr(user, c.UserSearch.NameAttr); ident.Name == "" {
 			missing = append(missing, c.UserSearch.NameAttr)
-		} else {
-			ident.Name = name
-			ident.Username = name
 		}
 	}
 
