eac1a3e4441cda15f5a20b7f9c28a29bff896f1b
Rui Yang <ruiya@vmware.com>
2020-09-26 01:50:45 -0400

    increase expirary delta time due to bcrypt encry/decryp cost
    Signed-off-by: Rui Yang <ruiya@vmware.com>

--- server/server_test.go
+++ server/server_test.go
@@ -267,7 +267,7 @@ func makeOAuth2Tests(clientID string, clientSecret string, now func() time.Time)
 						return t1.Sub(t2) < within
 					}
 
-					if !timeEq(token.Expiry, expectedExpiry, time.Second) {
+					if !timeEq(token.Expiry, expectedExpiry, time.Second*3) {
 						return fmt.Errorf("expected expired_in to be %s, got %s", expectedExpiry, token.Expiry)
 					}
 
